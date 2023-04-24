f21755169f6fd51ce82f883ddc7f6f00e13624ef
Mohammad Aljariri <moe.jariri@gmail.com>
2023-04-24 17:17:23 +0200

    First commit, first update

---
+++ VgameCards.js
@@ -0,0 +1,37 @@
+const cards = [
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "spiderman", img: "spiderman.jpg" },
+  { name: "superman", img: "superman.jpg" },
+  { name: "the avengers", img: "the-avengers.jpg" },
+  { name: "thor", img: "thor.jpg" },
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "spiderman", img: "spiderman.jpg" },
+  { name: "superman", img: "superman.jpg" },
+  { name: "the avengers", img: "the-avengers.jpg" },
+  { name: "thor", img: "thor.jpg" },
+];
+
+const infos = [
+  {
+    category: "stretch",
+    good: "Stretching - Stretching can help increase flexibility and reduce the risk of injury.",
+    bad: "Not stretching - Not stretching can lead to muscle stiffness and injury.",
+  },
+];
+// Exporting the cards variable
+
+export default cards;
